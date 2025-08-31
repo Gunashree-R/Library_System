540c60fa08ee1c3de5e22c2cf1cadb75cdf287f6
Gunashree-R <gunashree1811@gmail.com>
2025-08-31 16:20:07 +0530

    created BookRepository.java

--- LibrarySystem/src/main/java/com/example/LibrarySystem/repository/BookRepository.java
+++ LibrarySystem/src/main/java/com/example/LibrarySystem/repository/BookRepository.java
@@ -1,1 +1,10 @@
+package com.example.LibrarySystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.LibrarySystem.entity.Book;
+
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
 
