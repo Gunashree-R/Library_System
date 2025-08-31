5327ec8a40644e3a7d48ebb255e28342b635b923
Gunashree-R <gunashree1811@gmail.com>
2025-08-31 16:14:05 +0530

    created BookController.java

--- LibrarySystem/src/main/java/com/example/LibrarySystem/controller/BookController.java
+++ LibrarySystem/src/main/java/com/example/LibrarySystem/controller/BookController.java
@@ -1,1 +1,51 @@
+package com.example.LibrarySystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.LibrarySystem.entity.Book;
+import com.example.LibrarySystem.repository.BookRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@CrossOrigin(origins = "http://localhost:3001") // allow React frontend
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            book.setIsbn(bookDetails.getIsbn());
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
 
